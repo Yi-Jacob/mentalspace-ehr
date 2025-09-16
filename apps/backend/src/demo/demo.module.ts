c5470ce2460e1ad902e5898cea138ba8ad2182ef
Yi-Jacob <jacobyideveloper@gmail.com>
2025-09-15 21:35:43 -0400

    delete unimported files

--- apps/backend/src/demo/demo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DemoController } from './demo.controller';
-import { PrismaService } from '../database/prisma.service';
-
-@Module({
-  controllers: [DemoController],
-  providers: [PrismaService],
-})
-export class DemoModule {} 
